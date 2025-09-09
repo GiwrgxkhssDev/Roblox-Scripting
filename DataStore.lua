30519ba92626020a7c0f14c6f3fcb3b2054dd971
GiwrgxkhssDev <gpappas268@gmail.com>
2025-09-09 21:41:49 +0300

    DataStore.lua

--- DataStore.lua
+++ DataStore.lua
@@ -1,3 +1,5 @@
+-- Data Save Script Made By GiwrgxkhssDev ! Please Dont Delete Anything From Here!
+
 local DataStoreService = game:GetService("DataStoreService")
 local Players = game:GetService("Players")
 local playerDataStore = DataStoreService:GetDataStore("PlayerDataStore")
